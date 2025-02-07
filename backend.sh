77941e4b76f314b9dbf0002566af6b9849b929ab
Vivek <vivekanandareddy7677@gmail.com>
2025-02-07 15:36:38 -0600

    Adding Schema to backend script

--- backend.sh
+++ backend.sh
@@ -7,6 +7,8 @@ cp backend.service /etc/systemd/system/backend.service
 
 useradd expense
 
+rm -rf /app
+
 mkdir /app
 
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
@@ -19,3 +21,7 @@ systemctl daemon-reload
 
 systemctl enable backend
 systemctl start backend
+
+dnf install mysql -y
+
+mysql -h 172.16.17.18 -uroot -pExpenseApp@1 < /app/schema/backend.sql
