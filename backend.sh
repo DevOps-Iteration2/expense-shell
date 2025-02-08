0e678d86683cf00812907bfef966f5477ce4278c
Vivek <vivekanandareddy7677@gmail.com>
2025-02-08 00:25:40 -0600

    Creating common functionality in a file

--- backend.sh
+++ backend.sh
@@ -1,56 +1,58 @@
-echo Disable default NodeJS version module
+source common.sh
+
+Print_Task_Heading "Disable default NodeJS version module"
 dnf module disable nodejs -y &>> /tmp/expense.log # output gets appended to the existing
 echo $?
 
-echo Enable NodeJS module
-dnf module enable nodejs:20 -y
+Print_Task_Heading "Enable NodeJS module"
+dnf module enable nodejs:20 -y &>> /tmp/expense.log
 echo $?
 
-echo Install NodeJS
+Print_Task_Heading "Install NodeJS"
 dnf install nodejs -y
 echo $?
 
-echo Copy Backend Service
+Print_Task_Heading "Copy Backend Service"
 cp backend.service /etc/systemd/system/backend.service
-$?
+echo $?
 
-echo Adding Application User
+Print_Task_Heading "Adding Application User"
 useradd expense
-$?
+echo $?
 
-echo Remove Old App Content
+Print_Task_Heading "Remove Old App Content"
 rm -rf /app
-$?
+echo $?
 
-echo App Directory
+Print_Task_Heading "App Directory"
 mkdir /app
-$?
+echo $?
 
-echo Download App Content
+Print_Task_Heading "Download App Content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-$?
+echo $?
 
-echo Extract App Content
+Print_Task_Heading "Extract App Content"
 cd /app
 unzip /tmp/backend.zip
-$?
+echo $?
 
-echo Install NodeJS dependencies
+Print_Task_Heading "Install NodeJS dependencies"
 cd /app
 npm install
-$?
+echo $?
 
 systemctl daemon-reload
 
-echo Enable backend service
+Print_Task_Heading "Enable backend service"
 systemctl enable backend
 systemctl start backend
-$?
+echo $?
 
-echo Install MYSQL Client
+Print_Task_Heading "Install MYSQL Client"
 dnf install mysql -y
-$?
+echo $?
 
-echo Load Schema
+Print_Task_Heading "Load Schema"
 mysql -h 172.16.17.18 -uroot -pExpenseApp@1 < /app/schema/backend.sql
-$?
+echo $?
